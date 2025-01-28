660a75f0d3a96b4e0d4b0009d8ea8e7ed0ba04a6
Juanito Tamboong <juanitotamboongjr@gmail.com>
2025-01-28 11:52:59 +0800

    commit

--- welcome-page/guide.js
+++ welcome-page/guide.js
@@ -4,14 +4,14 @@ import { getFirestore, collection, query, where, getDocs } from "https://www.gst
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 
 // Firebase Configuration
-const firebaseConfig = { 
-    apiKey: "AIzaSyDXQCFoaCSWsCV2JI7wrOGZPKEpQuNzENA",
-    authDomain: "student-org-5d42a.firebaseapp.com",
-    projectId: "student-org-5d42a",
-    storageBucket: "student-org-5d42a.appspot.com",
-    messagingSenderId: "1073695504078",
-    appId: "1:1073695504078:web:eca07da6a1563c46e0829f"
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyCLEKn7V0CQMSMUYdybbeYcu5bwZICrBLQ",
+    authDomain: "student-org-backup.firebaseapp.com",
+    projectId: "student-org-backup",
+    storageBucket: "student-org-backup.firebasestorage.app",
+    messagingSenderId: "287364565723",
+    appId: "1:287364565723:web:ce0e52c5c862c8c4451afe"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
